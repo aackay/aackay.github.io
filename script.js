3909aa6affc172a728f3c22aaaff1d0ec038edb5
aackay <49598752+aackay@users.noreply.github.com>
2022-09-23 09:52:19 -0700

    added easter-egg

--- script.js
+++ script.js
@@ -42,3 +42,38 @@ window.onload = () => {
 
 		});
 }
+
+
+
+
+
+// adam easter-egg
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+const target = "adam".split("");
+let current = [];
+let enabled = false;
+
+document.body.onkeypress = e => {
+	if (enabled) {
+		return
+	}
+	current.push(e.key);
+	if (arraysEqual(current, target)) {
+		document.body.innerHTML += "<style>*{background: url('https://i.imgur.com/5P0FT9x.jpeg'); background-size: cover}</style>";
+		enabled = true;
+	} else if (current.length >= target.length) {
+		current = [];
+	} else if (current[current.length - 1] != target[current.length - 1]) {
+		current = [];
+	}
+}
